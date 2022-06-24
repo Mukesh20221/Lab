56aa47a3a69ce619fb2ecc8ee8a068fc292c9655
Mukesh Verma <107830305+Mukesh20221@users.noreply.github.com>
2022-06-24 11:37:07 +0530

    Mukesh Verma

--- MultiplicationFloatNumber.java
+++ MultiplicationFloatNumber.java
@@ -2,12 +2,12 @@ package Day4;
 import java.util.*;
 public class MultiplicationFloatNumber {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first number");
-		float firstno = sc.nextFloat();
-		System.out.println("Enter second number");
-		float seco = sc.nextFloat();
-		sc.close();
+		Scanner sc=new Scanner(System.in);//Scanner object
+		System.out.println("Enter first number");//Print message
+		float firstno = sc.nextFloat();//Take user input
+		System.out.println("Enter second number");//Print message
+		float seco = sc.nextFloat();//Take user input
+		sc.close();//Close scanner object
 		System.out.println("Multiplication of two number: "+(firstno * seco));
 	}
 }
