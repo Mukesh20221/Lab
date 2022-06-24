7870b6ded637487b94066d39f7e7836d03d0a334
Mukesh Verma <107830305+Mukesh20221@users.noreply.github.com>
2022-06-24 10:57:47 +0530

    Update Add2Number.java

--- Add2Number.java
+++ Add2Number.java
@@ -2,12 +2,12 @@ package Day4;
 import java.util.*;
 public class Add2Number {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first number");
-		int firstno = sc.nextInt();
-		System.out.println("Enter second number");
-		int secno = sc.nextInt();
-		sc.close();
-		System.out.println("Addition of two number: "+(firstno +secno ));
+		Scanner sc=new Scanner(System.in);//Scanner object
+		System.out.println("Enter first number");//Print message
+		int firstno = sc.nextInt();//Take user input
+		System.out.println("Enter second number");//Print message
+		int secno = sc.nextInt();//Take user input
+		sc.close();//Close scanner object
+		System.out.println("Addition of two number: "+(firstno +secno ));//Print result
 	}
 }
