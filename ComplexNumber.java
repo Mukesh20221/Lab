5ca19ad2506ee6b58fad168bc1ca6cdbfb5b5268
Mukesh Verma <107830305+Mukesh20221@users.noreply.github.com>
2022-06-24 11:12:15 +0530

    Mukesh Verma

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,14 +1,18 @@
 package Day4;
 public class ComplexNumber {
+	//Varable for real and imaginary parts of complex numbers
 	double real, img;
+	//constructor to initialize the complex number
 	 ComplexNumber(double r, double i){
 			this.real = r;
 			this.img = i;
 		   }
 	 public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2) {
+		 //creating a temporary complex number to hold the sum of two numbers
 		 ComplexNumber temp = new ComplexNumber(0, 0);
 		 temp.real = c1.real + c2.real;
 	        temp.img = c1.img + c2.img;
+		 //returning the output complex number
 	        return temp;
 	 }
 	 public static void main(String args[]) {
