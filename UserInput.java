46ba1f913e90b7593eb2b5f9e0d01119698c3982
Mukesh Verma <107830305+Mukesh20221@users.noreply.github.com>
2022-06-24 11:43:33 +0530

    Mukesh Verma

--- UserInput.java
+++ UserInput.java
@@ -2,10 +2,10 @@ package Day4;
 import java.util.Scanner;
 public class UserInput {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Please enter any name");
-		String name = sc.nextLine();
-		sc.close();
-		System.out.println("Your entered name: "+name);
+		Scanner sc=new Scanner(System.in);//Scanner object
+		System.out.println("Please enter any name");//Print message
+		String name = sc.nextLine();//Take user input
+		sc.close();//Close scanner object
+		System.out.println("Your entered name: "+name);//Print result
 	}
 }
